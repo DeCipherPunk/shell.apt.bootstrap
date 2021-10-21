51fefb33ff509bc03578c74c82823beb3f053586
DeCipherPunk <decipherpunk@protonmail.com>
2021-10-22 00:12:03 +0530

    fix paths and few things

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 echo "Downloading required scripts"
 git clone https://github.com/DeCipherPunk/shell.apt.update.git /opt/bootstrap/shell.apt.update
 git clone https://github.com/DeCipherPunk/shell.apt.upgrade.git /opt/bootstrap/shell.apt.upgrade
-https://github.com/DeCipherPunk/shell.apt.dependecies.git /opt/bootstrap/shell.apt.dependencies
+git clone https://github.com/DeCipherPunk/shell.apt.dependecies.git /opt/bootstrap/shell.apt.dependencies
 echo "Making scripts executable"
 sudo chmod +x /opt/bootstrap/shell.apt.update/run.sh
 sudo chmod +x /opt/bootstrap/shell.apt.upgrade/run.sh
