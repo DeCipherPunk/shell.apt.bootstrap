ac93f662c8be8ec50a76193d491772843c4205b8
DeCipherPunk <decipherpunk@protonmail.com>
2021-10-22 00:10:54 +0530

    fix paths and few things

--- run.sh
+++ run.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 echo "Downloading required scripts"
-git clone https://github.com/DeCipherPunk/shell.apt.update.git ~/bootstrap/shell.apt.update
-git clone https://github.com/DeCipherPunk/shell.apt.upgrade.git ~/bootstrap/shell.apt.upgrade
-git clone https://github.com/DeCipherPunk/shell.apt.dependencies.git ~/bootstrap/shell.apt.dependencies
+git clone https://github.com/DeCipherPunk/shell.apt.update.git /opt/bootstrap/shell.apt.update
+git clone https://github.com/DeCipherPunk/shell.apt.upgrade.git /opt/bootstrap/shell.apt.upgrade
+https://github.com/DeCipherPunk/shell.apt.dependecies.git /opt/bootstrap/shell.apt.dependencies
 echo "Making scripts executable"
-sudo chmod +x ~/bootstrap/shell.apt.update/run.sh
-sudo chmod +x ~/bootstrap/shell.apt.upgrade/run.sh
-sudo chmod +x ~/bootstrap/shell.apt.dependencies/run.sh
+sudo chmod +x /opt/bootstrap/shell.apt.update/run.sh
+sudo chmod +x /opt/bootstrap/shell.apt.upgrade/run.sh
+sudo chmod +x /opt/bootstrap/shell.apt.dependencies/run.sh
 echo "Run Apt Update"
-./bootstrap/shell.apt.update/run.sh
+/opt/bootstrap/shell.apt.update/run.sh
 echo "Run Apt Upgrade"
-./bootstrap/shell.apt.upgrade/run.sh
+/opt/bootstrap/shell.apt.upgrade/run.sh
 echo "Installing Dependecies"
-./bootstrap/shell.apt.dependencies/run.sh
+/opt/bootstrap/shell.apt.dependencies/run.sh
 
 # Config Books after this
 echo "Bootstrap Completed successfully"
